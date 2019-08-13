d2496fbf400a36cefab87fe924632cf7584c4944
Helge Heß <devteam@zeezide.de>
2019-08-13 13:21:31 +0200

    Swift 5 manifest
    ...

---
+++ Package@swift-5.swift
@@ -0,0 +1,13 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+  name: "WebPackMiniS",
+	
+  products: [
+    .library(name: "WebPackMiniS", targets: [ "WebPackMiniS" ])
+  ],
+  targets: [
+    .target(name: "WebPackMiniS")
+  ]
+)
